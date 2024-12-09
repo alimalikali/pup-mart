f1bef331ae3fdb5ca9741d8cd5e0e4144f29ce54
ALI ZULFIQAR <alimalikali1928@gmail.com>
2024-12-09 13:20:03 +0500

    eslint issue fixed again (✔ No ESLint warnings or errors) was using unused var

--- frontend/src/store/redux.tsx
+++ frontend/src/store/redux.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { Provider } from "react-redux";
-import globalReducer from "../store/index";
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { persistReducer, persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+import globalReducer from "../store/index";
 import { api } from "./api";
 
 
